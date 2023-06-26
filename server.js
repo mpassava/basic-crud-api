a488698f8e672245e0d4f5225a657945d5cb8957
mpassava <mpassava@gmail.com>
2023-06-25 23:57:40 -0400

    connected app to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const uri = ''; // INSERT MONGO DB URI
 
 app.get('/', (req, res) => {
     res.send('Hello NODE API');
-})
-
-app.listen(3000, () => {
-    console.log('Node API app is running on port 3000')
 });
 
+async function runApp() {
+    try {
+        const connect = await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+        app.listen(3000, () => {
+            console.log('Node API app is running on port 3000')
+        });
+    } catch (err){
+        console.error(`${err.name} - ${err.message}`);
+    }
+};
+
+runApp();
