762dbb94f447c6bec05c66c274e43253ccec6f6e
mpassava <mpassava@gmail.com>
2023-06-26 22:39:28 -0400

    added product schema

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const Product = require('./models/productModel');
 const uri = ''; // INSERT MONGO DB URI
+const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello NODE API');
+});
+
+app.post('/product', async(req, res) => {
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
 });
 
 async function runApp() {
